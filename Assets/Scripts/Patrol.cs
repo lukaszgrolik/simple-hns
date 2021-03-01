127a27b4e5d1081cefa9cd6eb9c20a790d2cfb8a
Łukasz Grolik <lukasz@grolik.pl>
2021-03-01 20:50:46 +0100

    Initial commit

---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class Patrol : MonoBehaviour
+{
+    private float radius = 5f;
+    // private float speed = 5f;
+    // private float waitTime = 1f;
+
+    // private Vector3 lastPos;
+    private Vector3 nextDestination;
+    // private float distance;
+    private float remDist;
+    private float lastRemDist;
+    private float sameRemDistTime = 0;
+
+    private NavMeshAgent navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.updateRotation = false;
+
+        GoToRandomDestination();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // travelProgress += navMeshAgent.speed * Time.deltaTime / distance;
+        remDist = Vector3.Distance(transform.position, nextDestination);
+
+        if (remDist == lastRemDist) {
+            sameRemDistTime += Time.deltaTime;
+        }
+        else {
+            lastRemDist = remDist;
+            sameRemDistTime = 0;
+        }
+
+        // transform.position = Vector3.Lerp(lastPos, nextDestination, travelProgress);
+
+        if (remDist < .5f || sameRemDistTime > 1f)
+        {
+            GoToRandomDestination();
+        }
+    }
+
+    void GoToRandomDestination()
+    {
+        // lastPos = transform.position;
+        nextDestination = GetRandomPos();
+        // distance = Vector3.Distance(lastPos, nextDestination);
+
+        navMeshAgent.SetDestination(nextDestination);
+    }
+
+    Vector3 GetRandomPos()
+    {
+        var randomPos = Random.insideUnitCircle * radius;
+
+        return transform.position + new Vector3(randomPos.x, 0, randomPos.y);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Gizmos.DrawWireSphere(lastPos, radius);
+        // Gizmos.DrawLine(lastPos, nextDestination);
+    }
+}
