c918103b3eaf060a073a22189e66c8b96af7f33a
Łukasz Grolik <lukasz@grolik.pl>
2022-03-30 15:34:01 +0200

    path generator

--- Assets/Scripts/Extensions.cs
+++ Assets/Scripts/Extensions.cs
@@ -69,9 +69,17 @@ public static class TransformExtensions
 {
     public static void DestroyChildren(this Transform transform)
     {
-        foreach (Transform child in transform)
+        for (int i = transform.childCount - 1; i >= 0; i--)
         {
-            GameObject.Destroy(child.gameObject);
+            GameObject.Destroy(transform.GetChild(i).gameObject);
+        }
+    }
+
+    public static void DestroyChildrenImmediate(this Transform transform)
+    {
+        for (int i = transform.childCount - 1; i >= 0; i--)
+        {
+            GameObject.DestroyImmediate(transform.GetChild(i).gameObject);
         }
     }
 }
---
+++ Assets/Scripts/MonoBehaviors/PathGenerator.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+[CustomEditor(typeof(PathGenerator))]
+public class PathGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var script = (PathGenerator)target;
+
+        if (DrawDefaultInspector())
+        {
+            // if (script.autoUpdate)
+            // {
+            //     script.Generate();
+            // }
+        }
+
+        if (GUILayout.Button("Generate")) script.Generate();
+        if (GUILayout.Button("Clear")) script.Clear();
+    }
+}
+#endif
+
+public class PathGenerator : MonoBehaviour
+{
+    [SerializeField] private Transform container;
+    [SerializeField] private List<GameObject> prefabs;
+    [SerializeField] private Gradient gradient;
+
+    public void UpdateFields(
+        Transform container,
+        List<GameObject> prefabs,
+        Gradient gradient
+    )
+    {
+        this.container = container;
+        this.prefabs = prefabs;
+        this.gradient = gradient;
+    }
+
+    public void Generate()
+    {
+        var objectDist = 1f;
+        var posDeviance = .25f;
+        var sizeDeviance = .2f;
+
+        var i = 0;
+        var lineFrom = Vector3.zero;
+        foreach (Transform point in transform)
+        {
+            var prefab = prefabs.Random();
+
+            if (i != 0)
+            {
+                var lineTo = point.position;
+
+                var pathFragmentDist = Vector3.Distance(lineFrom, lineTo);
+                var objectsCount = Mathf.FloorToInt(pathFragmentDist / objectDist);
+
+                for (int j = 0; j < objectsCount; j++)
+                {
+                    var basePos = lineFrom + (lineTo - lineFrom).normalized * objectDist * j;
+                    var randomCirclePos = (Random.insideUnitCircle * posDeviance).ToVector3();
+                    var pos = basePos + randomCirclePos;
+                    var rot = Quaternion.Euler(0, Random.Range(0f, 360f), 0);
+                    var size = Random.Range(1f - sizeDeviance, 1f + sizeDeviance);
+                    var color = gradient.Evaluate(Random.value);
+
+                    var obj = Instantiate(prefab, pos, rot);
+                    obj.transform.SetParent(container);
+                    obj.transform.localScale = Vector3.one * size;
+
+                    var spriteRend = obj.GetComponentInChildren<SpriteRenderer>();
+                    spriteRend.color = color;
+                }
+            }
+
+            lineFrom = point.position;
+
+            foreach (Transform child in point)
+            {
+                var pathGen = child.GetComponent<PathGenerator>();
+
+                if (pathGen != null)
+                {
+                    pathGen.UpdateFields(
+                        container,
+                        prefabs,
+                        gradient
+                    );
+                    pathGen.Generate();
+                }
+            }
+
+            i += 1;
+        }
+    }
+
+    public void Clear()
+    {
+        container.DestroyChildrenImmediate();
+    }
+
+#if UNITY_EDITOR
+    void OnDrawGizmos()
+    {
+
+        var i = 0;
+        var prevPos = Vector3.zero;
+        foreach (Transform child in transform)
+        {
+            var pos = child.position;
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(pos, .25f);
+
+            if (container.childCount == 0 && i != 0)
+            {
+                Gizmos.color = Color.grey;
+                Gizmos.DrawLine(prevPos, pos);
+            }
+
+            prevPos = pos;
+
+            i += 1;
+        }
+    }
+#endif
+}
